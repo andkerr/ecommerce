c2ffb11e1090bd961852d698c8302a3c2aecdf9c
Andrew Kerr <andrewghkerr@gmail.com>
2021-09-19 16:44:51 -0400

    adjust static file settings

--- commerce/settings.py
+++ commerce/settings.py
@@ -130,7 +130,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # URL that handles the media served from MEDIA_ROOT, used for managing stored files.
 # https://docs.djangoproject.com/en/3.2/ref/settings/#media-url
